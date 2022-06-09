998ee77bcf9bd53bc383c51ec752f26c857c78af
fatih <fthhbsr@gmail.com>
2022-06-10 01:02:36 +0300

    fix

--- app.js
+++ app.js
@@ -4,27 +4,38 @@ const displayDown = document.querySelector(".display-down");
 
 let displayUpValue = 0;
 let displayDownValue = 0;
-let operator;
-let operatorPrev;
+let isControl = true;
+let operator = [];
+
+// let operatorPrev;
 displayDown.innerHTML = displayDownValue;
 
+//click any button
 keys.addEventListener("click", (e) => {
-  //boşluk tıklandığında işlem yapmaması için
+  //so that it doesn't take action when the space is clicked
   if (e.target.parentElement.classList.contains("box")) {
     // console.log(e.target);
     //write to the number
     if (e.target.parentElement.classList.contains("number")) {
+      if (!isControl) {
+        // "=" işlemi yaptıktan sonra resetlemek için
+        displayDownValue = 0;
+        displayDown.innerHTML = "";
+      }
       displayDownValue = +(displayDown.innerHTML + e.target.innerHTML);
       console.log(displayDownValue);
+      displayDown.innerHTML = displayDownValue;
+      isControl = true;
     }
     //to make the current number positive or negative
     if (e.target.innerHTML == "±") {
       console.log("+-");
       displayDownValue *= -1;
       console.log(displayDownValue);
+      displayDown.innerHTML = displayDownValue;
     }
 
-    displayDown.innerHTML = displayDownValue;
+    // displayDown.innerHTML = displayDownValue;
     // point button
     if (e.target.innerHTML == ".") {
       console.log(e.target.innerHTML);
@@ -37,37 +48,52 @@ keys.addEventListener("click", (e) => {
     //operators buttons
     if (e.target.parentElement.classList.contains("operator")) {
       if (e.target.innerHTML == "x") {
-        operator = "*";
+        operator.push("*");
       } else if (e.target.innerHTML == "÷") {
-        operator = "/";
+        operator.push("/");
       } else {
-        operator = e.target.innerHTML;
+        operator.push(e.target.innerHTML);
       }
 
       if (displayUp.innerHTML) {
-        displayUpValue = process(
-          displayUpValue,
-          displayDownValue,
-          operatorPrev
-        );
-        displayUp.innerHTML = displayUpValue + operator;
+        if (displayDownValue) {
+          displayUpValue = process(
+            displayUpValue,
+            displayDownValue,
+            operator[operator.length - 2]
+          );
+          displayUp.innerHTML = displayUpValue + operator[operator.length - 1];
+        } else {
+          displayUp.innerHTML = displayUpValue + operator[operator.length - 1];
+        }
+
         displayDown.innerHTML = "";
         displayDownValue = 0;
       } else {
+        // if (displayDownValue != 0 && typeof displayDownValue == "number") {
+        // }
         displayUpValue = displayDownValue;
         displayDownValue = 0;
-        displayUp.innerHTML = displayDown.innerHTML + operator;
+        displayUp.innerHTML =
+          displayDown.innerHTML + operator[operator.length - 1];
         displayDown.innerHTML = "";
       }
-      operatorPrev = operator;
+
+      //   operatorPrev = operator;
       console.log(operator);
+      //   console.log(operatorPrev);
     }
     // result(=) button
     if (e.target.innerHTML == "=") {
       if (displayUp.innerHTML) {
-        displayDownValue = process(displayUpValue, displayDownValue, operator);
+        displayDownValue = process(
+          displayUpValue,
+          displayDownValue,
+          operator[operator.length - 1]
+        );
         displayDown.innerHTML = displayDownValue;
         displayUp.innerHTML = "";
+        isControl = false;
       }
     }
     // reset button
@@ -84,6 +110,7 @@ keys.addEventListener("click", (e) => {
 // arithmetic process function
 const process = function (val1, val2, ops) {
   let result;
+  console.log(ops);
   switch (ops) {
     case "*":
       result = val1 * val2;
@@ -99,7 +126,9 @@ const process = function (val1, val2, ops) {
       break;
   }
   if (result % 1 != 0) {
+    console.log(result, typeof result);
     result = result.toFixed(2);
   }
+
   return result;
 };
